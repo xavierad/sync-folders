51f7b1d845b4c54fdd5e19ec0cc17d1a62752f39
xavier <xavierdias24@gmail.com>
2023-07-24 18:15:57 +0200

    finishing sync

---
+++ syncronizer/syncronizer.py
@@ -0,0 +1,109 @@
+from datetime import datetime
+import logging
+import os
+import shutil
+import time
+from typing import List
+
+
+class Syncronizer:
+    def __init__(
+            self, 
+            source_path: str, 
+            target_path: str, 
+            logger: logging.Logger, 
+            interval: int = 10,
+            expiration_time: int = 360
+) -> None:
+        self.source_path = source_path
+        self.target_path = target_path
+
+        if not os.path.exists(target_path):
+            os.makedirs(target_path)
+        
+        self.logger = logger
+        self.interval = interval
+        self.expiration_time = expiration_time
+        
+        self.logger.info('Syncronizer setup!')
+        self.logger.info(f'Folder <{self.source_path}> will be syncronized to <{self.target_path}> every {self.interval} second(s)')    
+
+    def syncronize(self) -> None:
+        
+        self.logger.info(f'Syncronizing started!')
+        self._sync()
+        while True:
+            time.sleep(self.interval)
+            try: 
+                self.logger.info('Checking for changes...')
+                self._sync()
+
+            except (KeyboardInterrupt, IOError, OSError, BaseException) as exception:
+                self.logger.info(f'Syncronizing finished!')
+                raise exception
+
+    def _is_same(self, source: str, target: str) -> bool:
+        source_stat = os.stat(source)
+        target_stat = os.stat(target)
+
+        is_same_size: bool = source_stat.st_size == target_stat.st_size
+        is_expired: bool = (target_stat.st_mtime - source_stat.st_mtime) > self.expiration_time
+
+        return is_same_size and not is_expired
+
+    def _sync(self) -> None:
+        connections: List[str] = []
+        for root, dirs, files in os.walk(self.source_path):
+            for dir in dirs:
+                source_item = os.path.join(root, dir)
+                item_in_replica = source_item.replace(self.source_path, self.target_path)
+
+                connections.append(dir)
+                if not os.path.exists(item_in_replica):
+                    shutil.copytree(source_item, item_in_replica)
+                    self.logger.info(f'Synced {source_item} -> {item_in_replica}')
+                elif not self._is_same(source_item, item_in_replica):
+                    shutil.rmtree(item_in_replica)
+                    shutil.copytree(source_item, item_in_replica)
+                    self.logger.info(f'Synced {source_item} -> {item_in_replica}')
+                else:
+                    self.logger.info(f'No changes detected in {source_item}')
+
+            
+            for file in files:
+                source_item = os.path.join(root, file)
+                item_in_replica = source_item.replace(self.source_path, self.target_path)
+
+                connections.append(file)
+                if not os.path.exists(item_in_replica):
+                    shutil.copyfile(source_item, item_in_replica)
+                    self.logger.info(f'Synced {source_item} -> {item_in_replica}')
+
+                elif not self._is_same(source_item, item_in_replica):
+                    os.remove(item_in_replica)                        
+                    shutil.copyfile(source_item, item_in_replica)
+                    self.logger.info(f'Synced {source_item} -> {item_in_replica}')
+                else:
+                    self.logger.info(f'No changes detected in {source_item}')
+
+        for _, dirs, files in os.walk(self.target_path):
+            for file in files:
+                replica_item: str = os.path.join(self.target_path, file)
+                if file not in connections:
+                    os.remove(replica_item)
+                    self.logger.info(f'Removed {item_in_replica}')
+
+            for dir in dirs:
+                replica_item: str = os.path.join(self.target_path, dir)
+
+                if dir not in connections:
+                    shutil.rmtree(replica_item)
+                    self.logger.info(f'Removed {item_in_replica}')
+
+
+
+            
+
+            
+
+            
